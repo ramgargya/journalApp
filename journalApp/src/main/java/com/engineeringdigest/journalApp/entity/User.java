16e66e01e69c89b8fa6a31eddf3275b0ad70922c
ramgargya <ramkrsharma2005@gmail.com>
2025-03-10 08:38:14 +0530

    created UserRepositoryImpl.java in which we used Criteria, MongoTemplate and query by these 3 we can custom the query and fetch the data from the database using our filters and also added 2 instances in the User.java email and sentimentAnalysis for the future increments of the project

--- journalApp/src/main/java/com/engineeringdigest/journalApp/entity/User.java
+++ journalApp/src/main/java/com/engineeringdigest/journalApp/entity/User.java
@@ -1,6 +1,8 @@
 package com.engineeringdigest.journalApp.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Document(collection = "users")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -21,6 +25,9 @@ public class User {
     @Indexed(unique = true)
     @NonNull
     private String userName;
+
+    private String email;
+    private Boolean sentimentAnalysis;
 
     @NonNull
     private String password;
